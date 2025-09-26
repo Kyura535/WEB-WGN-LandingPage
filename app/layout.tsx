84e0559d7612d3cfc484ca8c017867ca35c17ad5
Kyura535 <umarabdaziz535@gmail.com>
2025-09-26 16:47:15 +0700

    Add Cookiebot script to layout for GDPR compliance

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+import { Script } from 'next/script'
 import './globals.css'
 import { ThemeProvider } from '@/components/theme-provider'
 
@@ -17,6 +18,13 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <body className={`${GeistSans.variable} ${GeistMono.variable} font-sans`}>
+        <Script 
+          id="Cookiebot" 
+          src="https://consent.cookiebot.com/uc.js" 
+          data-cbid="9a4d8d2b-293d-4cfb-8b98-39726126457b" 
+          type="text/javascript" 
+          strategy="beforeInteractive"
+        />
         <ThemeProvider>
           {children}
         </ThemeProvider>
