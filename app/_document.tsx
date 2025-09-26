d2075e088acae9265b94106c79796c31c457f39b
Kyura535 <umarabdaziz535@gmail.com>
2025-09-26 13:28:55 +0700

    Add Google Analytics gtag script to _document.tsx

--- app/_document.tsx
+++ app/_document.tsx
@@ -1,10 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 import type { DocumentContext, DocumentInitialProps } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-NQNX9KN9HY"
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-NQNX9KN9HY');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
